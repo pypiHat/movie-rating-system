6086c5072ac95b1cf93eb18cca8465bdabd05928
rootx_dev <rootx_dev@protonmail.com>
2023-04-07 10:36:30 +0330

    server initiated user.routes created

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+require('dotenv').config()
 
 
+const user = require('./routes/user.route')
 
-class
+require('./routes')(app) // register routes
+
+app.use('api/v1/', user)
+
+
+const port = process.env.port || 3000
+app.listen(port, () => {
+    console.log(`server running on port ${port}.`);
+})
